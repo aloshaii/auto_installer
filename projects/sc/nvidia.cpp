3fe5ffd8662f046ce7dd38df2a8cc147e0eec176
Aloshai <103289606+aloshaii@users.noreply.github.com>
2025-11-06 16:49:12 -0500

    Fix command execution checks for NVIDIA installation

--- projects/sc/nvidia.cpp
+++ projects/sc/nvidia.cpp
@@ -9,9 +9,11 @@ void installNvidiaDrivers() {
     int ret = std::system("sudo pacman -Syu nvidia nvidia-utils nvidia-settings");
     int rak = std::system("sudo nvidia-xconfig");
     int rus = std::system("sudo pacman -S nvidia-dkms");
-    int ruks = std::system("sudo nvidia-smi -pm 1");
+    int ruks = std::system("sudo nvidia-smi -pm 1 &&");
+    int rr = std::system("sudo nvidia-smi -lgc 0,0");
 
-    if (ret && rak && rus && ruks == 1) {
+
+    if (ret && rak && rus && ruks && rr == 1) {
         std::cout << "[SUCCESS] The NVIDIA driver was successfully installed.\n";
     } else {
         std::cerr << "[ERROR] The NVIDIA driver could not be installed.(Try Again)\n"; 
